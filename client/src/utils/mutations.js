22bfbecedd67e5d5b92f5dfecf1c1d399ab345f1
melvin <melvinrf@Outlook.com>
2021-08-10 11:43:37 -0500

    added more resolvers

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,13 @@
 import { gql } from "@apollo/client";
 
-export const ADD_USERS = gql`
-  mutation createUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      _id
-      username
-      email
-      password
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,10 @@
+const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 
 const resolvers = {
   Query: {
-    me: async () => {
-      return await User.find({}).populate("books");
-    },
-  },
-  Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      return await User.create({ username, email, password });
+    me: async (parent, args, context) => {
+      return await User.findOne({ _id: context.user._id });
     },
   },
 };
