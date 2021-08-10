b06fcbd602ee08ca1f0057823ab9ea29211532e5
melvin <melvinrf@Outlook.com>
2021-08-10 11:49:45 -0500

    fixed syntax errors in server resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,6 +7,31 @@ const resolvers = {
       return await User.findOne({ _id: context.user._id });
     },
   },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email: email });
+
+      if (!user) throw new AuthenticationError("Can't find user");
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) throw new AuthenticationError("Invalid password");
+
+      const token = signToken(user);
+      return token, user;
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({
+        username: username,
+        email: email,
+        password: password,
+      });
+
+      if (!user) return "User not found";
+      const token = signToken(user);
+      return token, user;
+    },
+  },
 };
 
 module.exports = resolvers;
