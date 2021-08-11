0c06dcac3fbc3739ef5c304d3fb01487b162ab6b
melvin <melvinrf@Outlook.com>
2021-08-11 10:36:51 -0500

    removed random P in code

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ async function startApolloServer() {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  if (process.env.NODE_ENV === "production") P;
+  if (process.env.NODE_ENV === "production");
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   server.applyMiddleware({ app, path: "/" });
